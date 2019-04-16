36ce66d51faad83209cb4c1769c8d4a5a6d923aa
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-16 15:54:29 +0100

    Removing compiled CSS and JS files
    Using a href linking to css files and javascript files from bootstraps remote server rather than downloading the compiled files and calling them locally in the main layout.

--- templates/layouts/main.php
+++ templates/layouts/main.php
@@ -5,9 +5,11 @@
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <title><?= $locals['pageTitle'] ?? 'Default Title' ?></title>
 
-        <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
-        <script src="./assets/js/jquery.js"></script> 
-        <script src="./assets/js/bootstrap.min.js"></script>
+        <link rel="stylesheet"href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
         </head>
     <body>
 
@@ -45,7 +47,7 @@
                     </li>
                     <li> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span>Sign In</a>
                         <ul class="dropdown-menu">
-                            <div style="width:300px;">
+                            <div style="width:300px;height:auto;">
                                 <div class="panel panel-primary">
                                     <div class="panel-heading">Log in</div>
                                     <div class="panel-heading">
@@ -57,7 +59,6 @@
                                         <a href="#" style="color:white; list-style:none;">Forgotten Password</a>
                                         <input type="submit" class="btn btn-success" style="float:right;" id="login" value="Login">
                                     </div>
-                                    <div class="panel-footer" id="e_msg"></div>
                                 </div>
                             </div>
                         </ul>

809f08f744dc82c0717efdaba2e3cea8a6d27b85
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-17 16:53:46 +0100

    Edit views for viewing coffee
    Adding bootstrap to ensure users can view coffees more clearly.

--- templates/views/view_coffees.php
+++ templates/views/view_coffees.php
@@ -38,3 +38,50 @@
     ?>
 
 </table>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    </head>
+    <body>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-2"></div>
+                <div class="col-md-8">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">Products</div>
+                        <div class="panel-body">
+                            <div class ="col-md-4">
+
+                                <?php
+                                foreach ($coffees['viewAllCoffees'] as $coffee) {
+                                    ?>
+
+                                    <div class="panel panel-info">
+                                        <div class="panel-heading"><?= $coffee->getCoffeeID() ?></div>
+                                        <div class="panel-body">
+                                            <img src="images/kenco-smooth.jpg" class="img-thumbnail"  alt="Responsive image">
+                                        </div>
+                                        <div class="panel-heading"><?= $coffee->getPrice() ?>
+                                            <button style="float:right;" class="btn btn-danger btn-xs">Add To Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php }
+                            ?>
+
+                        </div>
+                    </div>
+                    <div class ="panel-footer">&copy; 2019</div>
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </div>
+</body>
+</html>

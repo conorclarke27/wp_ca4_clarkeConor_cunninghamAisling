9ff977b4b08514c325cbc9ccf61f85ac51a49679
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-18 09:40:22 +0100

    GET Route for view-orders
    Added a GET route for view-orders.

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 
   // Define some routes. Here: requests to / will be
   // processed by the controller at controllers/Home.php
-  $app->GET('/',        'Home');
-  $app->GET('/view_coffees',        'ViewCoffees');
+  $app->GET('/',              'Home');
+  $app->GET('/view-coffees',  'ViewCoffees');
+  $app->GET('/view-orders',   'ViewOrders');
 
   // Process the request
   $app->dispatch();

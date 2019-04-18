f2cff28595e2f96387b7fde6b92363a1aac928a5
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-18 15:35:29 +0100

    Removed unused code
    Removed code that was displayed the rows in the database as the
    bootstrap now displayed the relevent information in pannels and divs.

--- templates/views/view-coffees.php
+++ templates/views/view-coffees.php
@@ -1,44 +1,5 @@
 <h2>View Coffees</h2>
 
-<table>
-    <tr>
-
-        <th><strong>coffee ID</strong></th>
-        <th><strong>Coffee Name</strong></th>
-        <th><strong>Supplier Name</strong></th>
-        <th><strong>Price</strong></th>
-        <th><strong>Quantity </strong></th>
-
-    </tr>
-
-
-
-    <ul>
-        <?php
-        foreach ($locals['viewAllCoffees'] as $coffee) {
-            ?>
-            <tr>
-                <td><?= $coffee->getCoffeeId() ?></td>
-                <td><?= $coffee->getCoffeeName() ?></td>
-                <td><?= $coffee->getSupplierName() ?></td>
-                <td><?= $coffee->getPrice() ?></td>
-                <td><?= $coffee->getQuantity() ?></td>
-
-                <td>
-                    <a class='btn' href='deletecoffee?coffeeID=<?= $coffee->getCoffeeId() ?>'> Delete coffee </a> 
-                </td>
-                <td>
-                    <a class='btn' href='editcoffee?coffeeID=<?= $coffee->getCoffeeId() ?>'> Edit coffee </a> 
-                </td>
-
-            </tr>
-        </ul>
-
-    <?php }
-    ?>
-
-</table>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -51,8 +12,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-1"></div>
-                <div class="col-md-2"></div>
-                <div class="col-md-9">
+                <div class="col-md-10">
                     <div class="panel panel-info">
                         <div class="panel-heading">Products</div>
                         <div class="panel-body">
@@ -81,6 +41,7 @@
 
                         </div>
                     </div>
+                    <div class="col-md-1"></div>
                 </div>
 </body>
 </html>

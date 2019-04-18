cac961872e99d7c03776c815140c42c56b4cf4ce
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-18 09:43:06 +0100

    Updating view file names in View controllers
    The name of the view files have been chnaged from underscores to
    hyphens. So the view_coffees.php has been updated to view_coffees.php.
    This has to be updated to ensure the controller was able to render the
    view file.

--- Controllers/ViewUsers.php
+++ Controllers/ViewUsers.php
@@ -4,7 +4,7 @@ return function($req, $res) {
     $db = \Rapid\Database::getPDO();
     require('./models/Users.php');
     $users = Users::findAll($db);
-    $res->render('main', 'view-Users', [
+    $res->render('main', 'view-users', [
         'pageTitle' => 'View Users',
         'viewAllUsers' => $users
     ]);

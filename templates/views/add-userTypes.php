52fcede046b28abc3fa51615e7d108733bdff6b7
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-18 13:16:06 +0100

    Created View for Add-UserTypes
    Created a form in the add-userTypes view.

---
+++ templates/views/add-userTypes.php
@@ -0,0 +1,10 @@
+<h2>Add User Types</h2>
+
+<form action='' method='post'>
+
+    <label for='typename'>User-type Name:</label>
+    <input type='text' id='typename' name='typename' value='<?= $typename['value']; ?>'>
+
+    <input type='submit' value='Submit'>
+
+</form>

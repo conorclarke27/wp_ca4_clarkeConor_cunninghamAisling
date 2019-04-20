650e46c8eefaabe6bb1d0b2a2f1d5784fdd74640
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-20 13:15:58 +0100

    Added EditUserGet Controller
    Controller gets ID from query string, uses this to get user
    info, and sends this info to the view in the render.

---
+++ controllers/EditUserGet.php
@@ -0,0 +1,15 @@
+<?php return function($req, $res) {
+
+  $db = \Rapid\Database::getPDO();
+  require('./models/User.php');
+
+  $user_id    = $req->query('user_id');
+  $user       = User::findOneById($user_id, $db);
+
+  $res->render('main', 'edit-users', [
+      // 'successMessage'    => $req->query('success') ? 'Successfully updated Order!!!' : '',
+      'pageTitle'         => 'Edit User',
+      'user'             => $user
+  ]);
+
+} ?>

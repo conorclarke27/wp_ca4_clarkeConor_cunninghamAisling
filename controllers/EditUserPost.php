39cc7361aeb46cef0690c1dc3c1e3977a7dee404
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-20 13:17:22 +0100

    Added EditUserPost Controller
    Creates a user and uses this info to update the user, then
    redirects to view-users.

---
+++ controllers/EditUserPost.php
@@ -0,0 +1,20 @@
+<?php return function($req, $res) {
+
+    $db = \Rapid\Database::getPDO();
+    require('./models/User.php');
+
+
+    $user = new User([
+        'user_id' => $req->query('user_id');
+        'type_id' => $req->body('type_id'),
+        'username' => $req->body('username'),
+        'password' => password_hash($req->body('password'),PASSWORD_BCRYPT,['cost' => 12]),
+        'email'=> $req->body('email'),
+        'supplier_name'=> $req->body('supplier_name')
+    ]);
+
+    User::update($db,$user);
+
+    $res->redirect('/view-users');
+
+} ?>

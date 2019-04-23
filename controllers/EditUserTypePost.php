94f6a4aadfcc668a9287f63b0d7e4ff403e0e805
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-23 14:46:36 +0100

    Added Controller for Edit User Type
    Added EditUserTypePost controller, creates a type object from
    info in the form, sends the type object to the update function,
    then redirects to the view-userTypes page.

---
+++ controllers/EditUserTypePost.php
@@ -0,0 +1,15 @@
+<?php return function($req, $res) {
+
+  $db = \Rapid\Database::getPDO();
+  require('./models/UserType.php');
+
+  $type = new UserType([
+    'type_id' => $req->body('type_id'),
+    'typename' => $req->body('typename')
+  ]);
+
+  UserType::update($db,$type);
+
+  $res->redirect('/view-userTypes');
+
+} ?>

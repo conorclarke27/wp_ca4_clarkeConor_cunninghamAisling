af29b886ea6b89b65e3e9bb2d40405728a00f669
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-23 09:51:53 +0100

    Added Controller for Delete User
    DeleteUser controller gets user_id from query string, then finds
    the user using findOneById function in User model, user object
    then deleted using delete function. User redirected to the
    view-users page.

---
+++ controllers/DeleteUser.php
@@ -0,0 +1,13 @@
+<?php
+    
+  $db = \Rapid\Database::getPDO();
+  require('./models/User.php');
+
+  $user_id    = $req->query('user_id');
+  $user       = User::findOneById($user_id, $db);
+
+  User::delete($db, $user);
+
+  $res->redirect('/view-users');
+
+?>

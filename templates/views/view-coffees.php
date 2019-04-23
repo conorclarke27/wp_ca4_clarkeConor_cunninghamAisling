32098f93f32d60b3631d40aae260084172d593db
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-23 21:19:41 +0100

    Beginning to implement a cart system
    In order for a user to purchase items they will be able to add items to
    a cart. The items will be addedd and displayed into the cart.

--- templates/views/view-coffees.php
+++ templates/views/view-coffees.php
@@ -11,8 +11,7 @@
 
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-1"></div>
-                <div class="col-md-10">
+                <div class="col-md-9">
                     <div class="panel panel-info">
                         <div class="panel-heading">Products</div>
                         <div class="panel-body">
@@ -28,11 +27,18 @@
                                         <div class="panel-body">
                                             <img src="./assets/images/kenco-smooth.jpg" class="img-thumbnail"  alt="Responsive image">
                                         </div>
-                                        <div class="panel-heading" style="height:80px;"><?= $coffee->getPrice() ?>
-                                            <a style="float:right;" class="btn btn-danger btn-xs" name="add_to_cart">Add To Cart</a>
+                                        <div class="panel-heading" style="height:80px;">
+                                        <?= $coffee->getPrice() ?>
+                                        <input style="height:30px; width:50px; float:right;" type='text' name='quantity' class="form-control quantity" id='qty'>
+                                        <a style="float:left;" class="btn btn-danger btn-xs add_to_cart" id="add_to_cart" name="add_to_cart" 
+                                        data-productid="<?= $coffee->getCoffeeId() ?>" 
+                                        data-productname="<?= $coffee->getCoffeeName() ?>" 
+                                        data-suppliername="<?= $coffee->getSupplierName() ?>"
+                                        data-price="<?= $coffee->getPrice() ?>">Add To Cart</a>
                                             <br><br>
-                                            <a style="float:left;" class='btn btn-danger btn-xs' href='DeleteCoffee?coffee_id=<?= $coffee->getCoffeeId() ?>'> Delete</a>
                                             <a style="float:right;" class="btn btn-success btn-xs" href='edit-coffees?coffee_id=<?= $coffee->getCoffeeId() ?>'> Edit </a>
+                                            <a style="float:left;" class='btn btn-danger btn-xs' href='DeleteCoffee?coffee_id=<?= $coffee->getCoffeeId() ?>'> Delete</a>
+                                            
                                             </div>
                                             </div>
                                 </div>
@@ -48,9 +54,59 @@
                 </div>
 
 
-                <div class="col-md-1"></div>
+                <div class="col-md-3">
+                <div class="panel panel-info">
+                <div class="panel-heading">Cart</div>
+                <div id="cart_details">
+                <h3 align="center"> Cart is Empty</h3></div></div>
 
+
+                
             </div>
         </div>
     </body>
 </html>
+
+<script>
+$(document).ready(function(){
+    $('.add_to_cart').click(function()
+    {
+        var product_id = $(this).data("productid");
+        var product_name = $(this).data("productname");
+        var supplier_name = $(this).data("suppliername");
+        var product_price = $(this).data("price");
+        var quantity = $('#qty').val();
+
+        
+
+        if(quantity != '' && quantity >0)
+        { 
+            //alert(quantity + "," + product_id + "," + product_name);
+            $.ajax({
+                url:"./controllers/ShoppingCart.php/add",
+                method:"POST",
+                data:{
+                product_id:product_id, 
+                product_name:product_name,
+                product_price:product_price,
+                quantiy:quantity},
+                success:function(data)
+                {
+                    alert("Product added to cart");
+                    $('#cart_details').html(data)
+
+                }
+            });
+
+        }
+        else
+        {
+            alert("Please eneter quantity");
+            
+        }
+
+    })
+
+});
+    
+</script>

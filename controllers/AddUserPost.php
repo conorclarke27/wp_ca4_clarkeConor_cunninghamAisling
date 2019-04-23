a9173c1acc27a9a0a50ae9a7e89d6cefb06dae21
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-23 15:22:39 +0100

    Checking passwords enetered match and user type
    The code checks if the user is an admin or not. If an admins the code
    will check the passwords match and if they contain the admin secret
    word. If a user, they program will just check the two passwords match
    and craete a user.

--- controllers/AddUserPost.php
+++ controllers/AddUserPost.php
@@ -3,19 +3,62 @@
     $db = \Rapid\Database::getPDO();
     require('./models/User.php');
 
-    $user = new User([
-        'type_id' => $req->body('type_id'),
-        'username' => $req->body('username'),
-        'password' => password_hash($req->body('password'),PASSWORD_BCRYPT,['cost' => 12]),
-        'email'=> $req->body('email'),
-        'supplier_name'=> $req->body('supplier_name')
-    ]);
+    $pass = $req->body('password');
+    $pass2 = $req->body('password2');
 
-    User::insert($db,$user);
+    if($req->body('type_id')=='2')
+    {
+        if(($pass == $pass2) && ($pass== 'admin'))
+        {
+            $user = new User([
+                'type_id' => $req->body('type_id'),
+                'username' => $req->body('username'),
+                'password' => password_hash($req->body('password'),PASSWORD_BCRYPT,['cost' => 12]),
+                'email'=> $req->body('email'),
+                'supplier_name'=> $req->body('supplier_name')
+            ]);
+        
+            User::insert($db,$user);
+        
+            
+            $res->render('main', 'add-user', [
+                'pageTitle' => 'Add Users',
+                'user' => $user
+                ]);
+        }
+        else
+        {
+            echo "not an  admin password";
+        }
+    }
+    else{
+        if($req->body('password') == $req->body('password2'))
+    {
+        $user = new User([
+            'type_id' => $req->body('type_id'),
+            'username' => $req->body('username'),
+            'password' => password_hash($req->body('password'),PASSWORD_BCRYPT,['cost' => 12]),
+            'email'=> $req->body('email'),
+            'supplier_name'=> $req->body('supplier_name')
+        ]);
+    
+        User::insert($db,$user);
+    
+        
+        $res->render('main', 'add-user', [
+            'pageTitle' => 'Add Users',
+            'user' => $user
+            ]);
+
+    }
+    else
+    {
+        echo "Passwords do not match";
+    }
+
+
+    }
 
     
-    $res->render('main', 'add-user', [
-        'pageTitle' => 'Add Users',
-        'user' => $user
-        ]);
+   
 }?>

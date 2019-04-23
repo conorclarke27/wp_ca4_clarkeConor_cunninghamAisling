04d129783b923fe80eaa1d432ddd474994a3d40d
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-23 14:15:05 +0100

    Added Edit and Delete buttons
    In order for the admin user to edit and delete coffees from the database
    the buttons must be present to take them to the delete or edit forms.

--- templates/views/view-coffees.php
+++ templates/views/view-coffees.php
@@ -28,26 +28,29 @@
                                         <div class="panel-body">
                                             <img src="./assets/images/kenco-smooth.jpg" class="img-thumbnail"  alt="Responsive image">
                                         </div>
-                                        <div class="panel-heading"><?= $coffee->getPrice() ?>
-                                            <button style="float:right;" class="btn btn-danger btn-xs" name="add_to_cart">Add To Cart</button>
-                                        </div>
-                                    </div>
-                                    <td>
-                                        <a class='btn' href='DeleteCoffee?coffee_id=<?= $coffee->getCoffeeId() ?>'> Delete Coffee </a> 
-                                    </td>
-                                    <td>
-                                        <a class='btn' href='edit-coffees?coffee_id=<?= $coffee->getCoffeeId() ?>'> Edit Coffee </a> 
-                                    </td>
-                            </div>
-                                       
-                        <?php }
+                                        <div class="panel-heading" style="height:80px;"><?= $coffee->getPrice() ?>
+                                            <a style="float:right;" class="btn btn-danger btn-xs" name="add_to_cart">Add To Cart</a>
+                                            <br><br>
+                                            <a style="float:left;" class='btn btn-danger btn-xs' href='DeleteCoffee?coffee_id=<?= $coffee->getCoffeeId() ?>'> Delete</a>
+                                            <a style="float:right;" class="btn btn-success btn-xs" href='edit-coffees?coffee_id=<?= $coffee->getCoffeeId() ?>'> Edit </a>
+                                            </div>
+                                            </div>
+                                </div>
+                            <?php }
                             ?>
-                            
 
                         </div>
-                    </div>
-                    <div class="col-md-1"></div>
-                </div>
-</body>
-</html>
 
+                    </div>
+
+
+
+                </div>
+
+
+                <div class="col-md-1"></div>
+
+            </div>
+        </div>
+    </body>
+</html>

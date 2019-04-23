0c8c50f861e3d32110e663becebc8e030c275921
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-23 14:42:25 +0100

    Added Controller for Edit User Type
    Added a EditUserTypeGet controller to get the type_id from the
    query string, uses findOneById function to get the type object,
    then uses render function to send object to edit-userTypes view.

---
+++ controllers/EditUserTypeGet.php
@@ -0,0 +1,15 @@
+<?php return function($req, $res) {
+
+  $db = \Rapid\Database::getPDO();
+  require('./models/UserType.php');
+
+  $type_id    = $req->query('type_id');
+  $type       = UserType::findOneById($db, $type_id);
+
+  $res->render('main', 'edit-userTypes', [
+      // 'successMessage'    => $req->query('success') ? 'Successfully updated Order!!!' : '',
+      'pageTitle'         => 'Edit User Type',
+      'type'             => $type
+  ]);
+
+} ?>

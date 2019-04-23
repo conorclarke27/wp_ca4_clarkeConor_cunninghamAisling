4db239c1d3eb8282506ee7bad490681868181eb5
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-23 09:39:56 +0100

    Added Controller for Delete Order
    DeleteOrder controller get order_id from query string, then
    finds order object from the order_id, and deletes that order
    object. User then redirected to view-orders.

---
+++ controllers/DeleteOrder.php
@@ -0,0 +1,13 @@
+<?php
+    
+  $db = \Rapid\Database::getPDO();
+  require('./models/Order.php');
+
+  $order_id    = $req->query('order_id');
+  $order       = Order::findOneById($db, $order_id);
+
+  Order::delete($db, $order);
+
+  $res->redirect('/view-orders');
+
+?>

a05971e7c0c1ba52565e9d1624dce4c5311c7ba1
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-23 14:17:16 +0100

    Add Edit and Delete buttons as well as bootstrap
    To allow the users to control orders there must be an option for them to
    edit and delete their orders. This is made possible with the functions
    behind the buttons.
    
    To view the form neatly, there has been bootstrap used to style the
    page.

--- templates/views/edit-orders.php
+++ templates/views/edit-orders.php
@@ -1,15 +1,57 @@
 <?php $order = $locals['order']; ?>
+<?php $coffees = $locals['viewAllCoffees']?>
+<br>
+<br>
+<br>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    </head>
+    <body>
 
-<h2>Edit Orders</h2>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-10">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">Edit Order</div>
+                        <div class="panel-body">
+                           <form action='' method='post'>
+                            <div class="col-md-8">
+                            <label for='user_id'>User ID:</label>
+                            <input type='text' id='user_id' name='user_id' value='<?= $order->getUserID(); ?>'class="form-control">
+                            </div>
+                            <div  class="col-md-8">
+                            <label for="coffee_id">Coffee Name:</label>
+                            <select name="coffee_id" id="coffee_id" class="form-control">
+                            <option value="" disabled selected>Select...</option>
+                            <?php foreach($coffees as $coffee) {?>                                                                                                           
+                            <option value="<?= $coffee->getCoffeeId()?>"><?= $coffee->getCoffeeName()?> </option>
+                            <?php } ?>
+                            </select>
+                            </div>
+                            <p><br/></p>
+                            <div class="row">
+                            <div class="col-md-12">
+                                <input style="float:right;" type='submit' value='Edit Order' class="btn btn-success btn-lg">
+                            </div>
+                            </div>
+                        </form>
+                        </div>
 
-<form action='' method='post'>
+                    </div>
 
-    <label for='user_id'>User ID:</label>
-    <input type='text' id='user_id' name='user_id' value='<?= $order->getUserID(); ?>'>
 
-    <label for='coffee_id'>Coffee ID:</label>
-    <input type='text' id='coffee_id' name='coffee_id' value='<?= $order->getCoffeeID(); ?>'>
 
-    <input type='submit' value='Submit'>
+                </div>
 
-</form>
+
+                <div class="col-md-1"></div>
+
+            </div>
+        </div>
+    </body>
+</html>

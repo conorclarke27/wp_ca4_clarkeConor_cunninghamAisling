2e12f815f967f578126215c27bc02ff4881958d5
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-29 12:56:31 +0100

    Added in Functions
    Added a isValidName - this checks for a regex match and returns
    a boolean, added a isValidEmail - uses the filter_var function
    to validate that it is an email returns a boolean.
    
    Changed isValidId to cast $id as an int.

--- lib/utils/ModelUtils.php
+++ lib/utils/ModelUtils.php
@@ -1,17 +1,28 @@
 <?php class ModelUtils{
 
-public static function isValidId($id) {
-  return is_int($id) && ($id >0);
-}
+  public static function isValidId($id) {
+    return (int)$id && ($id >0);
+  }
 
-public static function isValidNumber($num) 
-{
-  return is_int($num) && ($num >= 0);
-}
+  public static function isValidNumber($num) {
+    return is_numeric($num) && ($num >= 0);
+  }
+
+  public static function isValidName($name) {
+    if(!preg_match("@^[A-Za-z.-]+(\s*[A-Za-z.-]+)*$@", $name)) {
+      return false;
+    }
+    return true;
+  }
 
 
-//public static function isValidEmail($email)
-
+  public static function isValidEmail($email) {
+    //return is_email($email); says that is_email is not a recognized function
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      return false;
+    }
+    return true;
+  }
 
 } ?>
 

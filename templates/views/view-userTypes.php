cbd27d78d0aba7a556d0f1ad0e9d36ef20ee4347
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-29 13:47:29 +0100

    Removed HTML tags
    Removed some of the tags as they were not necessary.

--- templates/views/view-userTypes.php
+++ templates/views/view-userTypes.php
@@ -1,13 +1,5 @@
-<h2></h2>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    </head>
-    <body>
-    <div class="row">
+    <h2>User Types</h2>
+    <div class="row" style="margin-top: 20px;">
     <?php foreach ($locals['viewAllUserTypes'] as $userType) { ?>
   <div class="col-sm-3">
     <div class="card">
@@ -24,6 +16,3 @@
   </div>
   <?php }?>
   </div>
-
-    </body>
-</html>

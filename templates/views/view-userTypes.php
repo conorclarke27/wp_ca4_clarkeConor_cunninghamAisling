44e72d43301ec3f54ec4f860810469e24456c289
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-30 21:00:16 +0100

    Formated the tags
    Formated the tags to attempt to make it more readable.

--- templates/views/view-userTypes.php
+++ templates/views/view-userTypes.php
@@ -6,11 +6,10 @@
       <div class="card-body">
         <h5 class="card-title">User Type: <?= $userType->getTypeName() ?></h5>
         <p class="card-text">Type ID: <?=  $userType->getTypeId() ?> </p>
-        </div>
-        <div class="card-footer">
+      </div>
+      <div class="card-footer">
         <a class='btn btn-success btn-xs' href="edit-userTypes?type_id=<?= $userType->getTypeId() ?>"> Edit</a>
         <a  class="btn btn-danger btn-xs" href="DeleteUserType?type_id=<?= $userType->getTypeId() ?>">Delete</a>
-        
       </div>
     </div>
   </div>

1c6973e322955109b5d370b20976acf13b30f80d
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-30 20:55:21 +0100

    Formated the tags
    Formated the tags to attempt to make it more readable.

--- templates/views/add-userTypes.php
+++ templates/views/add-userTypes.php
@@ -1,46 +1,37 @@
 <h2>Create a user type</h2>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    </head>
-    <body>
-    <body>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
       
-      <div class="row">
+<div class="row">
   <div class="col-sm-1"></div>
   <div class="col-sm-9">
   <div class="card text-white bg-info mb-3">
     <div class="card-body">
       <h5 class="card-title">Create a user type</div></h5>
       <p class="card-text">
-                           <form action='' method='post'>
-                            <div class="col-sm-8">
-                            <label for='typename'>User-type Name:</label>
-                            <input type='text' id='typename' name='typename' value='<?= $typename['value']; ?>'class="form-control">
-                            </div>
-                            
-                            <p><br/></p>
-                            <div class="row">
-                            <div class="col-sm-11">
-                                <input style="float:right;" type='submit' value='Create User Type' class="btn btn-success btn-lg">
-                            </div>
-                            </div>
-                        </form>
-                        </div>
-
-                    </div>
-
-
-
-                </div>
-
-
-                <div class="col-md-1"></div>
-
+        <form action='' method='post'>
+            <div class="col-sm-8">
+                <label for='typename'>User-type Name:</label>
+                <input type='text' id='typename' name='typename' value='<?= $typename['value']; ?>'class="form-control">
             </div>
-        </div>
-    </body>
+            <p><br/></p>
+            <div class="row">
+                <div class="col-sm-11">
+                    <input style="float:right;" type='submit' value='Create User Type' class="btn btn-success btn-lg">
+                </div>
+            </div>
+        </form>
+    </div>
+    </div>
+</div>
+<div class="col-md-1"></div>
+</div>
+</div>
+</body>
 </html>

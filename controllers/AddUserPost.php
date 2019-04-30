9de3087ec0d1eb3e9816db28daa56678612c910d
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-30 16:47:17 +0100

    Redirecting user once they sign up
    Once the form submits, the user is now registered and is redirected to
    the view coffees page.

--- controllers/AddUserPost.php
+++ controllers/AddUserPost.php
@@ -3,13 +3,7 @@
     $db = \Rapid\Database::getPDO();
     require('./models/User.php');
 
-    $pass = $req->body('password');
-    $pass2 = $req->body('password2');
-
-    if($req->body('type_id')=='2')
-    {
-        if(($pass == $pass2) && ($pass== 'admin'))
-        {
+    
             $user = new User([
                 'type_id' => $req->body('type_id'),
                 'username' => $req->body('username'),
@@ -19,46 +13,7 @@
             ]);
         
             User::insert($db,$user);
+            $res->redirect('/view-coffees');
         
-            
-            $res->render('main', 'add-user', [
-                'pageTitle' => 'Add Users',
-                'user' => $user
-                ]);
-        }
-        else
-        {
-            echo "not an  admin password";
-        }
-    }
-    else{
-        if($req->body('password') == $req->body('password2'))
-    {
-        $user = new User([
-            'type_id' => $req->body('type_id'),
-            'username' => $req->body('username'),
-            'password' => password_hash($req->body('password'),PASSWORD_BCRYPT,['cost' => 12]),
-            'email'=> $req->body('email'),
-            'supplier_name'=> $req->body('supplier_name')
-        ]);
     
-        User::insert($db,$user);
-    
-        
-        $res->render('main', 'add-user', [
-            'pageTitle' => 'Add Users',
-            'user' => $user
-            ]);
-
-    }
-    else
-    {
-        echo "Passwords do not match";
-    }
-
-
-    }
-
-    
-   
-}?>
+    }?>

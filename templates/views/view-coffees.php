3d180521547ccf638968dba7f50851a35e18487c
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-30 22:42:41 +0100

    Updated submit button id
    Ad the id on the button was add_to_cart, it has been updated to match
    the call in the javascript file.

--- templates/views/view-coffees.php
+++ templates/views/view-coffees.php
@@ -3,18 +3,20 @@
 <div class="row" style="margin-top: 20px;">
     <?php foreach ($locals['viewAllCoffees'] as $coffee) {
         ?>
-        <div class="col-sm-3">
+        <div class="col-sm-3" id='shopping_cart'>
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title"><span class="fas fa-coffee"></span> <?= $coffee->getCoffeeName() ?></h5>
+                <form id='shopping_cart' action='' method='post'>
+                    <h5 class="card-title" id='coffeename'><span class="fas fa-coffee"></span> <?= $coffee->getCoffeeName() ?></h5>
                     <p class="card-text"> <img src="./assets/images/coffee.png" class="img responsive img-thumbnail"  alt="Coffee"></p>
                     <p></p>
                         <?php if ($_SESSION["LOGGED_IN"]) { ?>
                           <div class="card-footer" style="height:150px;">
-                            <h5>€<?= $coffee->getPrice() ?></h5>
-                            <input style="height:30px; width:50px; float:right;" type='text' name='quantity' class="form-control quantity" id="<?= $coffee->getCoffeeId() ?>">
-                            <a style="float:left;" class="btn btn-danger btn-xs add_to_cart" id="add_to_cart" name="add_to_cart">Add To Cart</a>
+                            <h5 id='price'>€<?= $coffee->getPrice() ?></h5>
+                            <input style="height:30px; width:50px; float:right;" type='text' name='quantity' class="form-control quantity" id="quantity">
+                            <a style="float:left;" class="btn btn-danger btn-xs add_to_cart" type='submit' id="submit" name="add_to_cart">Add To Cart</a>
                         <?php } else { ?>
+                        </div>
                           <div class="card-footer" style="height:100px;">
                             <h5>€<?= $coffee->getPrice() ?></h5>
                             <h5>You must log in or sign up to purchase</h5>

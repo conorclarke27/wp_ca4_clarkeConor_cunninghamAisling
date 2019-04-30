b642f13fe0c08ea577626329ed58c18b59e5cb73
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-30 22:12:25 +0100

    Added error message checking
    Added in FormUtils to check the content in the forms, gives back
    a value and is_valid, if not valid sends error messages in the
    render through the locals associative array.

--- controllers/AddCoffeesPost.php
+++ controllers/AddCoffeesPost.php
@@ -3,19 +3,51 @@
     $req->sessionStart();
     $db = \Rapid\Database::getPDO();
     require('./models/Coffee.php');
+    require('./lib/utils/FormUtils.php');
 
-    $coffee = new Coffee([
-        'coffee_name' => $req->body('coffee_name'),
-        'supplier_name' => $req->body('supplier_name'),
-        'price' => $req->body('price'),
-        'quantity' => $req->body('quantity')
-    ]);
+    $form_error_messages = [];
 
-    Coffee::insert($db, $coffee);
+    $form_was_posted = $req->body('coffee_name') !== NULL;
 
-    $res->render('main', 'add-coffees', [
-        'pageTitle' => 'Add Coffees',
-        'coffee'   => $coffee
-    ]);
+    $coffee_name    = FormUtils::getPostName($req->body('coffee_name'));
+    $supplier_name  = FormUtils::getPostName($req->body('supplier_name'));
+    $price          = FormUtils::getPostFloat($req->body('price'));
+    $quantity       = FormUtils::getPostInt($req->body('quantity'));
+
+    if (!$coffee_name['is_valid']) {
+        $form_error_messages['coffee_name'] = 'Coffee name required';
+    }
+
+    if (!$supplier_name['is_valid']) {
+        $form_error_messages['supplier_name'] = 'Supplier name required';
+    }
+
+    if (!$price['is_valid']) {
+        $form_error_messages['price'] = 'Price required';
+    }
+
+    if (!$quantity['is_valid']) {
+        $form_error_messages['quantity'] = 'Quantity required';
+    }
+
+    if(!$form_was_posted || count($form_error_messages) > 0) {
+        $res->render('main', 'add-coffees', [
+            'pageTitle' => 'Add Coffees',
+            'form_error_messages'   => $form_error_messages
+        ]);
+    }
+
+    else {
+        $coffee = new Coffee([
+            'coffee_name' => $coffee_name['value'],
+            'supplier_name' => $supplier_name['value'],
+            'price' => $price['value'],
+            'quantity' => $quantity['value']
+        ]);
+
+        Coffee::insert($db, $coffee);
+
+        $res->redirect('/add-coffees');
+    }
 
 } ?>

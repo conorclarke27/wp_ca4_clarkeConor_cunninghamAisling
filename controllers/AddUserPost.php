b7ca8ca31992ac7e7f3209954a0d67c0752690b7
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-30 20:05:53 +0100

    When adding a user set session with name
    To add a personal touch to the nav bar, the user name will be stored in
    session which will then be displayed beside the login button.

--- controllers/AddUserPost.php
+++ controllers/AddUserPost.php
@@ -7,7 +7,7 @@
 
     
             $user = new User([
-                'type_id' => $req->body('type_id'),
+                'type_id' => 1,
                 'username' => $req->body('username'),
                 'password' => password_hash($req->body('password'),PASSWORD_BCRYPT,['cost' => 12]),
                 'email'=> $req->body('email'),
@@ -16,6 +16,8 @@
         
             User::insert($db,$user);
             $req->sessionSet('LOGGED_IN',TRUE);
+            $req->sessionSet('Name',$user->getUsername());
+
             $res->redirect('/view-coffees');
         
     

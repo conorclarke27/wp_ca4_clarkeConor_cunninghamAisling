a1bdae82387496bdf57425b9edd7afb55ef3b96e
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-30 13:37:21 +0100

    Adding stylesheet and icons
    To add more detail to the website there has been a plug in for
    fontawesome used. This allows the icons on the nav bar to be displayed.

--- templates/layouts/main.php
+++ templates/layouts/main.php
@@ -6,6 +6,7 @@
         <title><?= $locals['pageTitle'] ?? 'Default Title' ?></title>
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.1.1.min.js">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -21,22 +22,23 @@
         <div class="collapse navbar-collapse" id="navbarsExample07">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="<?= SITE_BASE_DIR ?>/">Home</a>
+              <a class="nav-link" href="<?= SITE_BASE_DIR ?>/"><span class="fas fa-home"></span> Home</a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="<?= SITE_BASE_DIR ?>/view-coffees">Products</a>
+              <a class="nav-link" href="<?= SITE_BASE_DIR ?>/view-coffees"><span class = "fas fa-mug-hot"></span> Products</a>
             </li>
             <li class="nav-item active dropdown">
-              <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Users</a>
+              <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="fas fa-users"></span> Users</a>
               <div class="dropdown-menu" aria-labelledby="dropdown07">
                 <a class="dropdown-item" href='<?= SITE_BASE_DIR ?>/add-user'>Sign Up</a>
                 <a class="dropdown-item" href='<?= SITE_BASE_DIR ?>/user-login' >Log In</a>
+                <a class="dropdown-item" href='<?= SITE_BASE_DIR ?>/admin-login' >Admin</a>
               </div>
             </li>
             
           </ul>
           <form class="form-inline my-2 my-md-0">
-          <a class="nav-link disabled" href=''>Cart <span class="badge badge-secondary">0</a>
+          <a class="nav-link disabled" href=''><span class="fas fa-shopping-cart"></span> Cart <span class="badge badge-secondary">0</a>
           </form>
         </div>
       </div>

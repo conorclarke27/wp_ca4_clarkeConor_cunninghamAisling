0ec38e210fc765e1c6b1dda333c9202fca230f01
Conor Clarke <conorjamesclarke@gmail.com>
2019-04-30 22:27:07 +0100

    Added error checking of forms
    Added in FormUtils to check the form for errors.

--- controllers/AddOrdersPost.php
+++ controllers/AddOrdersPost.php
@@ -3,17 +3,40 @@
  $req->sessionStart();
   $db = \Rapid\Database::getPDO();
   require('./models/Order.php');
+  require('./lib/utils/FormUtils.php');
 
-  $order = new Order([
-    'user_id' => $req->body('user_id'),
-    'coffee_id' => $req->body('coffee_id')
-  ]);
+  $form_was_posted = [];
 
-  Order::insert($db, $order);
+  $user_id = FormUtils::getPostInt($req->body('user_id'));
+  $coffee_id = FormUtils::getPostInt($req->body('coffee_id'));
 
-  $res->render('main', 'add-orders', [
-    'pageTitle' => 'Add Orders',
-    'order'   => $order
-  ]);
+  if (!$user_id['is_valid']) {
+    $form_error_messages['user_id'] = 'User ID required';
+  }
+
+  if (!$coffee_id['is_valid']) {
+      $form_error_messages['coffee_id'] = 'Coffee ID required';
+  }
+
+  if(!$form_was_posted || count($form_error_messages) > 0) {
+      $res->render('main', 'add-orders', [
+        'pageTitle' => 'Add Orders',
+        'form_error_messages'   => $form_error_messages
+      ]);
+  }
+
+  else {
+    $order = new Order([
+      'user_id' => $user_id['value'],
+      'coffee_id' => $coffee_id['value']
+    ]);
+  
+    Order::insert($db, $order);
+  
+    $res->render('main', 'add-orders', [
+      'pageTitle' => 'Add Orders',
+      'order'   => $order
+    ]);
+  }
 
 } ?>

dcfc18e2a5bf3960f61ec4feaa42d603ff7784cb
Aisling Cunningham <Aislingc98@icloud.com>
2019-04-30 22:02:40 +0100

    Check session is set for Admin
    As the admin can only access this page, the session is checked t see if
    admin is set. If not, the user is redirected to error page.

--- controllers/AddUserTypesGet.php
+++ controllers/AddUserTypesGet.php
@@ -1,9 +1,17 @@
 <?php return function($req, $res) {
-
+  $req->sessionStart();
+  $admin = $req->session("Admin");
+  
+  if($admin)
+  {
     $res->render('main', 'add-userTypes', [
     'pageTitle' => 'Add User Types'
     
   ]);
+}
+  else
+  {
+    $res->render('main', '404', []);
+  }
   
-
-} ?>
+  } ?>

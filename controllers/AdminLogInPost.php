69699a301ef5075dfa013cbe05eaa478c4d402e6
Conor Clarke <conorjamesclarke@gmail.com>
2019-05-01 12:54:46 +0100

    Added error checks to Log In
    Added in code to check for empty email or password, formutils
    check for the email, also redirects back to page if something
    happens.

--- controllers/AdminLogInPost.php
+++ controllers/AdminLogInPost.php
@@ -1,44 +1,45 @@
 <?php return function($req, $res) {
-    
-    require('./models/User.php');
     $req->sessionStart();
-
     $db = \Rapid\Database::getPDO();
-    $email = $req->body('email');
-    $userInput = $req->body('password');
+    require('./models/User.php');
+    require('./lib/utils/FormUtils.php');
+
+    if(!$req->body('email') === NULL || !$req->body('password') === NULL) {
+        $email = FormUtils::getPostEmail($req->body('email'));
+        $userInput = $req->body('password');
+        
+        if(!$email['is_valid'] || $email === NULL) {
+            $user = User::findOneByEmail($email,$db);
     
-    $user = User::findOneByEmail($email,$db);
-    
-    if(!($user->getUserId() === NULL))
-    {
-        $userType = $user->getUserType();
-        $name     = $user->getUsername();
-        $id       = $user->getUserId();
-        if($userType == '2')
-        {
-            $userPassword =  $user->getPassword();
-            $valid       = password_verify($userInput,$userPassword);
-            if($valid)
+            if(!($user->getUserId() === NULL))
             {
-                $req->sessionSet('LOGGED_IN',TRUE);
-                $req->sessionSet('Name',$name);
-                $req->sessionSet('Id', $id);
-                $req->sessionSet('Admin',TRUE);
-                $res->redirect('/admin-menu');
+                $userType = $user->getUserType();
+                $name     = $user->getUsername();
+                $id       = $user->getUserId();
+                if($userType == '2')
+                {
+                    $userPassword =  $user->getPassword();
+                    $valid       = password_verify($userInput,$userPassword);
+                    if($valid)
+                    {
+                        $req->sessionSet('LOGGED_IN',TRUE);
+                        $req->sessionSet('Name',$name);
+                        $req->sessionSet('Id', $id);
+                        $req->sessionSet('Admin',TRUE);
+                        $res->redirect('/admin-menu');
+                    }
+                }
             }
         }
-        
-        
     }
+   
     else
     {
+        $error['error'] = "Invalid email or password";
         $req->sessionSet('LOGGED_IN',FALSE);
-        echo('User null');
+        $res->render('main', 'admin-login', [
+            'form_error_messages' => $error
+        ]);
     }
 
-    
-}
-
-
-
-?>
+} ?>

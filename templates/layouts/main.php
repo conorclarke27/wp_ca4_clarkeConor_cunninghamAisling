4d79bd9cfbe209d86c2eeda7724b5ea5741e6de9
Aisling Cunningham <Aislingc98@icloud.com>
2019-05-01 11:46:53 +0100

    Adding buttons to nav bar for orders and adminmenu
    Creating buttons to allow logged in users to see their orders. Admin
    mneu button to allow admin to see menu.

--- templates/layouts/main.php
+++ templates/layouts/main.php
@@ -6,9 +6,9 @@
         <title><?= $locals['pageTitle'] ?? 'Coffee Cart' ?></title>
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">  
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>      
+        <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">  
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script> -->
+        <!-- <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>       -->
         <script src="https://code.jquery.com/jquery-3.1.1.min.js">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -32,7 +32,17 @@
                     <li class="nav-item active">
                         <a class="nav-link" href="<?= SITE_BASE_DIR ?>/view-coffees"><span class = "fas fa-mug-hot"></span> Products</a>
                     </li>
-                    <?php if (!($_SESSION['LOGGED_IN'] === TRUE || $_SESSION['Admin'] === TRUE )) { ?>
+                    <?php if ($_SESSION['Admin'] === TRUE) { ?>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="<?= SITE_BASE_DIR ?>/admin-menu"><span class = "fas fa-user-cog"></span> Admin Menu</a>
+                    </li>
+                    <?php }?>
+                    <?php if ($_SESSION['LOGGED_IN'] === TRUE) { ?>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="<?= SITE_BASE_DIR ?>/view-orders"><span class = "fas fa-user-cog"></span> Orders</a>
+                    </li>
+                    <?php }?>
+                    <?php if (!($_SESSION['LOGGED_IN'] === TRUE)) { ?>
                      <li class="nav-item active dropdown">
                         <a class="nav-link dropdown-toggle"  id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="fas fa-users"></span> Users</a>
                         <div class="dropdown-menu" aria-labelledby="dropdown07">

c2395f116a3b67f363ad29c99d78e5a1d1b1138d
Conor Clarke <conorjamesclarke@gmail.com>
2019-05-01 14:50:33 +0100

    Removed Div tag
    Removed a div tag to make bootstrap look more appealing.

--- templates/views/admin-login.php
+++ templates/views/admin-login.php
@@ -34,7 +34,6 @@
       </div>
     </div>
   </div>
-  </div>
 
 <div class="alert alert-warning alert-dismissible" role="alert">
   <strong>Holy guacamole!</strong> You should check in on some of those fields below.

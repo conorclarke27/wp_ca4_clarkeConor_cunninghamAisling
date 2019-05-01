a72ab1d74e7e5c4776e33ae32d116e4d80c0e038
Aisling Cunningham <Aislingc98@icloud.com>
2019-05-01 11:48:57 +0100

    Changing height of footer
    Decreasing footer height.

--- templates/views/view-orders.php
+++ templates/views/view-orders.php
@@ -7,7 +7,7 @@
         <h5 class="card-title">Order: <?= $order->getOrderID() ?></h5>
         <p class="card-text"> User ID: <?= $order->getUserID() ?></p>
         <p class="card-text"> Coffee ID: <?= $order->getCoffeeID() ?></p>
-        <div class="card-footer" style="height:150px;">
+        <div class="card-footer" style="height:100px;">
           <a  class='btn btn-success btn-xs' href='edit-orders?order_id=<?= $order->getOrderID() ?>'> Edit</a>
           <a class="btn btn-danger btn-xs" href='DeleteOrder?order_id=<?= $order->getOrderID() ?>'>Delete</a>
         </div>

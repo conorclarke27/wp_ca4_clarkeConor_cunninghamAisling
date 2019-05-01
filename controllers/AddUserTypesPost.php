a6000d8d705e130424745e755c0163db192ff76e
Conor Clarke <conorjamesclarke@gmail.com>
2019-05-01 15:16:23 +0100

    Added error checking
    Added form error checking for user types

--- controllers/AddUserTypesPost.php
+++ controllers/AddUserTypesPost.php
@@ -1,27 +1,44 @@
 <?php return function($req, $res) {
 
-$req->sessionStart();
+  $req->sessionStart();
   $db = \Rapid\Database::getPDO();
   require('./models/UserType.php');
+  require('./lib/utils/FormUtils.php');
 
   $admin = $req->session("Admin");
 
-  if($admin)
-  {
-  $type = new UserType([
-    'typename' => $req->body('typename')
-  ]);
+  if($admin) {
 
-  UserType::insert($db, $type);
+    $form_error_messages = [];
 
-  $res->render('main', 'add-userTypes', [
-    'pageTitle' => 'Add User Types',
-    'type'   => $type
-  ]);
-}
-else
-{
-    $res->render('main', '404', []);
-}
+    $form_was_posted = $req->body('typename') !== NULL;
+
+    $typename = FormUtils::getPostName($req->body('typename'));
+
+    if (!$typename['is_valid']) {
+      $form_error_messages['typename'] = 'Invalid Type name';
+    }
+
+    if(!$form_was_posted || count($form_error_messages) > 0) {
+      $res->render('main', 'add-userTypes', [
+        'pageTitle' => 'Add User Types',
+        'form_error_messages'   => $form_error_messages
+      ]);
+    }
+
+    else {
+      $type = new UserType([
+        'typename' => $req->body('typename')
+      ]);
+  
+      UserType::insert($db, $type);
+  
+      $res->redirect('/view-userTypes');
+    }
+
+  }
+  else {
+      $res->render('main', '404', []);
+  }
 }
  ?>

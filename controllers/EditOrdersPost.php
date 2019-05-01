da8b9fd6b78be44e545d5e74cdc6373d7e541875
Conor Clarke <conorjamesclarke@gmail.com>
2019-05-01 10:56:50 +0100

    Added error checks for form
    Added in error checks for the form inputs.

--- controllers/EditOrdersPost.php
+++ controllers/EditOrdersPost.php
@@ -2,14 +2,46 @@
     $req->sessionStart(); 
     $db = \Rapid\Database::getPDO();
     require('./models/Order.php');
+    require('./models/Coffee.php');
+    require('./lib/utils/FormUtils.php');
 
-    $order = new Order([
-        'order_id' => $req->query('order_id'),
-        'user_id' => $req->body('user_id'),
-        'coffee_id' => $req->body('coffee_id')
-    ]);
+    $form_was_posted = [];
+  
+    $form_was_posted = $req->query('order_id') !== NULL;
+  
+    $order_id = FormUtils::getPostInt($req->query('order_id'));
+    $user_id = FormUtils::getPostInt($req->body('user_id'));
+    $coffee_id = FormUtils::getPostInt($req->body('coffee_id'));
 
-    Order::update($db, $order);
+    if (!$user_id['is_valid']) {
+      $form_error_messages['user_id'] = 'Invalid user ID (e.g. 2)';
+    }
+  
+    if (!$coffee_id['is_valid']) {
+        $form_error_messages['coffee_id'] = 'You must pick a Coffee name';
+    }
+
+    if(!$form_was_posted || count($form_error_messages) > 0) {
+        $order = Order::findOneById($db, $order_id['value']);
+        $coffees = Coffee::findAll($db);
+        $res->render('main', 'edit-orders', [
+            'pageTitle' => 'Edit Orders',
+            'form_error_messages'   => $form_error_messages,
+            'viewAllCoffees' => $coffees,
+            'order' => $order
+        ]);
+    }
+
+    else {
+        $order = new Order([
+            'order_id' => $order_id['value'],
+            'user_id' => $user_id['value'],
+            'coffee_id' => $coffee_id['value']
+        ]);
     
-    $res->redirect("/view-orders");
+        Order::update($db, $order);
+        
+        $res->redirect("/view-orders");
+    }
+    
 } ?>

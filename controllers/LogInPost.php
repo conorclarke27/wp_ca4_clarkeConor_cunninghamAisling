5773b6801ef62519310e51fd79f7dccad05ab57e
Conor Clarke <conorjamesclarke@gmail.com>
2019-05-01 14:53:54 +0100

    Added in error checking
    Added in error checking for the log in form, user makes their
    way through multiple if statements before redirected upon success
    or if fail they render the user login page with the error message.

--- controllers/LogInPost.php
+++ controllers/LogInPost.php
@@ -1,42 +1,40 @@
 <?php return function($req, $res) {
-    
-    require('./models/User.php');
     $req->sessionStart();
+    $db = \Rapid\Database::getPDO();  
+    require('./models/User.php');
+    require('./lib/utils/FormUtils.php');
+    
+    $error['error'] = "Invalid email or password";
 
-    $db = \Rapid\Database::getPDO();
-    $email = $req->body('email');
-    $userInput = $req->body('password');
-    
-    $user = User::findOneByEmail($email,$db);
-    
+    if($req->body('email') !== NULL || $req->body('password') !== NULL) {
+        $email = FormUtils::getPostEmail($req->body('email'));
+        $userInput = $req->body('password');
+        
+        if($email['is_valid'] || $email !== NULL) {
+            $user = User::findOneByEmail($email['value'],$db);
 
+            if($user !== NULL)
+            {
+                $userPassword =  $user->getPassword();
+                $name         =  $user->getUsername();
+                $id           =  $user->getUserId();
+            
+                $valid       = password_verify($userInput,$userPassword);
 
-    if(!($user->getUserId() === NULL))
-    {
-        $userPassword =  $user->getPassword();
-        $name         =  $user->getUsername();
-        $id           =  $user->getUserId();
-    
-        $valid       = password_verify($userInput,$userPassword);
-    
-        if($valid)
-        {
-            $req->sessionSet('LOGGED_IN',TRUE);
-            $req->sessionSet('Name', $name);
-            $req->sessionSet('Id', $id);
-            $res->redirect('/');
-    
+                if($valid)
+                {
+                    $req->sessionSet('LOGGED_IN',TRUE);
+                    $req->sessionSet('Name', $name);
+                    $req->sessionSet('Id', $id);
+                    $res->redirect('/');
+                }
+            }
         }
-    
     }
-    else
-    {
-        $req->sessionSet('LOGGED_IN',FALSE);
-        $res->redirect('/user-login');
-    }
+
+    $req->sessionSet('LOGGED_IN',FALSE);
+    $res->render('main', 'user-login', [
+            'form_error_messages' => $error
+    ]);
     
-}
-
-
-
-?>
+} ?>

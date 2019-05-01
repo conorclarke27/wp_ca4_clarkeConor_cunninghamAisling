4c93725e51d6cb2537a9668febb84de0b77e326c
Conor Clarke <conorjamesclarke@gmail.com>
2019-05-01 15:43:43 +0100

    Added error checking
    Added form error checking to check that the type name is
    actually a name.

--- controllers/EditUserTypePost.php
+++ controllers/EditUserTypePost.php
@@ -1,16 +1,36 @@
 <?php return function($req, $res) {
-  $req->sessionStart();
-
+  $req->sessionStart(); 
   $db = \Rapid\Database::getPDO();
   require('./models/UserType.php');
+  require('./lib/utils/FormUtils.php');
 
-  $type = new UserType([
-    'type_id' => $req->body('type_id'),
-    'typename' => $req->body('typename')
-  ]);
+  $form_error_messages = [];
+  
+  $form_was_posted = $req->query('type_id') !== NULL;
+  $type_id = FormUtils::getPostInt($req->query('type_id'));
+  $typename = FormUtils::getPostName($req->body('typename'));
+  if (!$typename['is_valid']) {
+    $form_error_messages['typename'] = 'Invalid Type name (e.g. Sales, Student Developers)';
+  }
 
-  UserType::update($db,$type);
+  if(!$form_was_posted || count($form_error_messages) > 0) {
+    $type    = UserType::findOneById($db, $type_id['value']);
+    $res->render('main', 'edit-userTypes', [
+        'pageTitle' => 'Edit Orders',
+        'form_error_messages'   => $form_error_messages,
+        'type' => $type
+    ]);
+  }
 
-  $res->redirect('/view-userTypes');
+  else {
+    $type = new UserType([
+      'type_id' => $type_id['value'],
+      'typename' => $typename['value']
+    ]);
+  
+    UserType::update($db,$type);
+  
+    $res->redirect('/view-userTypes');
+  }
 
 } ?>

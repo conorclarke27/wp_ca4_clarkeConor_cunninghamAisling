a5cf4a5e2242ef926ff700b48b1fda3c47346f2b
Conor Clarke <conorjamesclarke@gmail.com>
2019-05-01 15:14:49 +0100

    Formated file
    Formated file to make it more readable.

--- controllers/AdminMenu.php
+++ controllers/AdminMenu.php
@@ -1,16 +1,16 @@
 <?php return function($req, $res) {
-$req->sessionStart();
-$admin = $req->session("Admin");
+    $req->sessionStart();
+    $admin = $req->session("Admin");
 
-if($admin)
-{
-    $res->render('main', 'admin-menu', [
-        'pageTitle' => 'Admin Menu'
-      ]);
-}
-else
-{
-    $res->render('main', '404', []);
-}
+    if($admin)
+    {
+        $res->render('main', 'admin-menu', [
+            'pageTitle' => 'Admin Menu'
+        ]);
+    }
+    else
+    {
+        $res->render('main', '404', []);
+    }
 }
  ?>

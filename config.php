1cdbfff977eaf6b2f8dd4a24c675282e7aa45c91
Conor Clarke <conorjamesclarke@gmail.com>
2019-10-03 13:32:25 +0100

    Upload of web project, was previously on DkIT gitlab.

---
+++ config.php
@@ -0,0 +1,26 @@
+
+
+<?php return [
+  
+  //config values needed for local host
+
+  'ENV'           => 'development',
+  'DATABASE_HOST' => '127.0.0.1',
+  'DATABASE_USER' => 'root',
+  'DATABASE_PASS' => '',
+  'DATABASE_NAME' => 'wp_ca4_clarkeconor_cunninghamaisling'
+
+  
+  //config values needed for remote host 
+
+  // 'ENV'           => 'development',
+  // 'DATABASE_HOST' => '127.0.0.1',
+  // 'DATABASE_USER' => 'id9485236_aisling',
+  // 'DATABASE_PASS' => '############',
+  // 'DATABASE_NAME' => 'id9485236_wp_ca4'
+
+] ?>
+
+
+
+
